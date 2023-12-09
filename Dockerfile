fe0ebde647e2ff02a1ca1232200a2ff4f2c4c0b9
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-12-09 17:27:10 +0100

    refactor: change job in .groovy

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
-FROM jenkins/jenkins:lts
+FROM jenkins/jenkins:latest
 USER root
 RUN apt-get update && \
-    apt-get install -y \
-    docker.io \
-    && rm -rf /var/lib/apt/lists/*
+    apt-get install -y make && \
+    apt-get install -y gcc && \
+    apt-get install -y libcriterion-dev && \
+    apt-get install -y gcovr
 
 COPY jenkins.yml /var/jenkins_home/jenkins.yml
 COPY job_dsl.groovy /usr/local/job_dsl.groovy
-
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
-ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
 
+ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
 ENV CASC_JENKINS_CONFIG /var/jenkins_home/jenkins.yml
 
 RUN jenkins-plugin-cli --plugin-file /usr/share/jenkins/ref/plugins.txt
--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,102 +1,35 @@
-pipelineJob('Whanos base images/whanos-python') {
-    displayName('whanos-python')
-    definition {
-        cps {
-            script('''
-                pipeline {
-                    agent any
-                    stages {
-                        stage('Checkout') {
-                            steps {
-                                checkout scm
-                            }
-                        }
-                        stage('Build Docker Image') {
-                            steps {
-                                script {
-                                    sh 'docker build -t whanos-python-base -f Dockerfile.base .'
-                                }
-                            }
-                        }
-                        stage('Build and Run Project') {
-                            steps {
-                                script {
-                                    sh 'docker run --rm -it whanos-python-base'
-                                }
-                            }
-                        }
-                    }
-                }
-            ''')
-        }
-    }
-    properties {
-        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: ''))
-    }
+folder('Whanos base images') {
+  description('Folder base images jobs.')
 }
 
+folder('PROJECTS') {
+  description('Folder base images jobs.')
+}
 
+freeStyleJob('Whanos base images/whanos-c') {
+    description('Build Docker Image triggered by GitHub push')
 
-// // Job pour construire toutes les images de base
-// pipelineJob('build-all-base-images') {
-//     displayName('Build All Base Images')
-//     definition {
-//         cps {
-//             script('''
-//                 build 'whanos-haskell'
-//                 build 'whanos-python'
-//                 // Ajoutez des lignes similaires pour d'autres langages
-//             ''')
-//         }
-//     }
-//     properties {
-//         buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: ''))
-//     }
-// }
+    wrappers {
+      preBuildCleanup()
+    }
 
-// // Job link-project
-// job('link-project') {
-//     displayName('Link Project')
-//     parameters {
-//         stringParam('projectName', '', 'Nom du projet à lier')
-//         // Ajoutez d'autres paramètres si nécessaire
-//     }
-//     steps {
-//         // Ajoutez les étapes nécessaires pour lier le projet à l'infrastructure Whanos
-//     }
-// }
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
 
-// // Jobs créés par le job link-project
-// pipelineJob('project-1') {
-//     displayName('Project 1')
-//     triggers {
-//         pollSCM('* * * * *') // Vérifie les modifications toutes les minutes
-//     }
-//     definition {
-//         cps {
-//             script('''
-//                 node {
-//                     stage('Checkout') {
-//                         // Étape pour récupérer le code source depuis le référentiel du projet 1
-//                         // git 'https://votre-repo-git.com/project-1.git'
-//                     }
-
-//                     stage('Containerize') {
-//                         // Étape pour containeriser l'application selon les spécifications Whanos
-//                         // sh 'docker build -t project-1 .'
-//                     }
-
-//                     stage('Deploy') {
-//                         // Étape pour déployer l'application dans un cluster Kubernetes si applicable
-//                         // Utilisez Kubernetes plugin ou kubectl pour le déploiement
-//                     }
-//                 }
-//             ''')
-//         }
-//     }
-//     properties {
-//         buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: ''))
-//     }
-// }
-
-// // Ajoutez des blocs similaires pour d'autres projets créés par le job link-project
+    steps {
+        scm {
+            git {
+                remote {
+                    url('${GIT_REPOSITORY_URL}')
+                    credentials('SSH_KEY_REPO')
+                }
+                branches('*/main')
+            }
+        }
+        shell('docker build -t whanos-c-standalone -f images/c/Dockerfile.standalone .')
+    }
+    triggers {
+        cron('* * * * *')
+    }
+}
